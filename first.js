a9490f9c95d2a2ed4113940364eb227d43d51cd4
ashish <yadavashish1519@gmail.com>
2026-02-18 12:19:33 +0530

    javascript

---
+++ first.js
@@ -0,0 +1,38 @@
+let a= "ashish";
+console.log("name =",a);
+
+let age=25;
+console.log(age);
+
+
+const country="india";
+console.log(country);
+
+let z=25;
+console.log("before",z);
+z=50;
+console.log("after",z);
+
+//change const variable value
+// const q=20;
+// console.log(q);
+// q=20;
+// console.log(q);
+//const can not change
+
+//typ eof string variable
+let rao ="nitin";
+console.log(typeof rao);
+
+
+// type of num variable
+let num=487;
+console.log(typeof num);
+
+// type of boolean
+let isnum=false;
+console.log(typeof isnum);
+
+// type of null
+ 
+console.log(typeof null);
